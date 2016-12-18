a292a5f67f7aa3d4c3f93a1e29cebdacb2bf6c44
Marcelo Santos <marcelosantosadm@gmail.com>
2016-12-18 15:02:57 +0000

    alteraÃõoes verÃao simplista com mudanÃnças html s

---
+++ js/curriculo.js
@@ -0,0 +1,31 @@
+(function($) {
+    "use strict";
+
+    // jQuery scrol das páginas - depende do jQuery Easing plugin
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+
+    // Fecha menu responsivo ao clicar neste
+    $('.navbar-collapse ul li a').click(function(){
+            $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+})(jQuery);
